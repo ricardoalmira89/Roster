9b2d155f45f6fa8653dfb63ecbb671eacb0bf8b3
cyber <ricardoalmira89@bnjm.cu>
2014-07-22 06:33:12 +0200

    Esquema creado. Entidades Creadas. Datos de Programas y Schedules insertados.

---
+++ Roster/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Roster.Model;
+using Roster.Forms;
+
+namespace Roster
+{
+    public partial class Form1 : Form
+    {
+        public AddStudentForm AddStudent = new AddStudentForm();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*Student student = new Student();
+            student.address = "calle 214a";
+            student.attachment = "123";
+            student.bussiness_phone = "8284350";
+            student.C__home_phone = "123";
+            student.city = "Lisa";
+            student.comments = "123";
+            student.company = "BNJM";
+            student.country = "cuba";
+            student.credit_trans = "0000";
+            student.email = "aaaa@bb.cc";
+            student.emergency_contact_name = "ec";
+            student.emergency_contact_phone1 = "123";
+            student.emergency_contact_phone2 = "123";
+            student.emergency_contact_phone3 = "123";
+            student.end_date = new DateTime(2014, 07, 01);
+            student.EO = "eo";
+            student.fax_number = "111";
+            student.first_name = "Ricardo";
+            //student.id = 1;
+            student.last_name = "Almira";
+            student.location = "location";
+            student.locker = "loker";
+            student.mobile_phone = "aaa";
+            student.notes = "no";
+            student.refered = "ref";
+            student.start_date = new DateTime(2014, 06, 01);
+            student.state = "state";
+            student.status = "aaa";
+            //student.Student_Schedule
+            student.web_page = "www.aa.bb";
+            student.ZIP = "qwe";
+
+            using (var dbCtx = new rosterEntities())
+            {
+                
+                //Add standard object into Standard DBset
+                dbCtx.Students.Add(student);
+                // call SaveChanges method to save standard into database
+                dbCtx.SaveChanges();
+            }
+            */
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddStudent.ShowDialog();
+        }
+    }
+}
---
+++ Roster/Forms/AddStudentForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Roster.Model;
+
+namespace Roster.Forms
+{
+    public partial class AddStudentForm : Form
+    {
+        rosterEntities context = new rosterEntities();
+        Roster.Model.Program Program;
+        Roster.Model.Schedule Schedule;
+        Roster.Model.Student_Schedule Student_Schedule;
+
+        public AddStudentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void AddStudentForm_Load(object sender, EventArgs e)
+        {
+            programBindingSource.DataSource = context.Programs.ToList();
+        }
+
+        private void programComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            scheduleBindingSource.DataSource = context.Schedules.ToList();
+            Program = context.Programs.First(c => c.id == (int)programComboBox.SelectedValue);
+            scheduleBindingSource.DataSource = Program.Schedules.ToList();
+        }
+
+        private void scheduleComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Schedule = Program.Schedules.First(s => s.id == (int)scheduleComboBox.SelectedValue);
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            /*Student student = new Student();
+            student.address = addressTextBox.Text;
+            student.attachment = attachmentTextBox.Text;
+            student.bussiness_phone = bussiness_phoneTextBox.Text;
+            student.C__home_phone = c__home_phoneTextBox.Text;
+            student.city = cityTextBox.Text;
+            student.comments = commentsTextBox.Text;
+            student.company = companyTextBox.Text;
+            student.country = countryTextBox.Text;
+            student.credit_trans = credit_transTextBox.Text;
+            student.email = emailTextBox.Text;
+            student.emergency_contact_name = emergency_contact_nameTextBox.Text;
+            student.emergency_contact_phone1 = emergency_contact_phone1TextBox.Text;
+            student.emergency_contact_phone2 = emergency_contact_phone2TextBox.Text;
+            student.emergency_contact_phone3 = emergency_contact_phone3TextBox.Text;
+            student.end_date = end_dateDateTimePicker.Value;
+            student.start_date = start_dateDateTimePicker.Value;
+            student.EO = eOTextBox.Text;
+            student.fax_number = fax_numberTextBox.Text;
+            student.first_name = first_nameTextBox.Text;
+            student.last_name = last_nameTextBox.Text;
+            student.location = locationTextBox.Text;
+            student.locker = lockerTextBox.Text;
+            student.mobile_phone = mobile_phoneTextBox.Text;
+            student.notes = notesTextBox.Text;
+            student.refered = referedTextBox.Text;
+            student.state = stateTextBox.Text;
+            student.status = stateTextBox.Text;
+            student.web_page = web_pageTextBox.Text;
+            student.ZIP = zIPTextBox.Text;*/
+
+            Student_Schedule = new Student_Schedule();
+            Student_Schedule.Schedule = Schedule;
+            Student_Schedule.Student = new Student();
+            //Student_Schedule.Student = student;
+            //Student_Schedule.schedule_id = Schedule.id;
+            //Student_Schedule.student_id = student.id;
+          
+
+            //student.Student_Schedule.Add(Student_Schedule);
+            
+            context.Student_Schedule.Add(Student_Schedule);
+            //context.Students.Add(student);
+            context.SaveChanges();
+            
+        }
+
+
+
+   }
+}
